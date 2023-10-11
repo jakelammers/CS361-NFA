d5b2d8eb468cea4cf64d5a13da176a7be313e529
Jake Lammers <jakelammers@u.boisestate.edu>
2023-10-10 23:31:03 -0600

    implemented given methods, needs work

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -1,63 +1,100 @@
 package fa.nfa;
 
+import java.util.LinkedHashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import fa.State;
 
 public class NFA implements NFAInterface {
 
+    private Set<NFAState> states;
+    private Set<NFAState> finalStates;
+    private NFAState startState;
+    private Set<Character> alphabet;
+
+    public NFA() {
+        startState = null;
+        states = new LinkedHashSet<>();
+        finalStates = new LinkedHashSet<>();
+        alphabet = new LinkedHashSet<>();
+    }
+
     @Override
     public boolean addState(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+        if(states.contains(getState(name))) {
+            return false;
+        }
+        NFAState newState = new NFAState(name);
+        states.add(newState);
+        return true;
     }
 
     @Override
     public boolean setFinal(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setFinal'");
+        for(NFAState c: states) {
+            if(c.getName().equals(name)) {
+                finalStates.add(c);
+                return true;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean setStart(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setStart'");
+        if(states.contains(getState(name))) {
+            startState = (NFAState) getState(name);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public void addSigma(char symbol) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addSigma'");
+        alphabet.add(symbol);
     }
 
     @Override
     public boolean accepts(String s) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+        NFAState currentState = startState;
+        for(int i=0; i<s.length(); i++) {
+            currentState = currentState.getToState(s.charAt(i));
+        }
+        if(finalStates.contains(currentState)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
     public Set<Character> getSigma() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSigma'");
+        return alphabet;
     }
 
     @Override
     public State getState(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getState'");
+        for(NFAState checking: states) {
+            if(Objects.equals(checking.getName(), name)) {
+                return checking;
+            }
+        }
+        return null;
     }
 
     @Override
     public boolean isFinal(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isFinal'");
+        for(NFAState c: finalStates) {
+            if(c.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean isStart(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isStart'");
+        return startState.getName().equals(name);
     }
 
     @Override
@@ -66,12 +103,14 @@ public class NFA implements NFAInterface {
         throw new UnsupportedOperationException("Unimplemented method 'getToState'");
     }
 
+    //FIXME
     @Override
     public Set<NFAState> eClosure(NFAState s) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'eClosure'");
     }
 
+    //FIXME
     @Override
     public int maxCopies(String s) {
         // TODO Auto-generated method stub
@@ -80,14 +119,87 @@ public class NFA implements NFAInterface {
 
     @Override
     public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTransition'");
+        if(!alphabet.contains(onSymb)) {return false;}
+        if(!states.contains(getState(fromState))) {return false;}
+        if(!states.contains(getState(toState))) {return false;}
+        for (NFAState state : states) {
+            if(state.getName().equals(fromState)) {
+                state.addToState(onSymb, (NFAState) getState(toState));
+                return true;
+            }
+        }
+        return false;
     }
 
+    //FIXME
     @Override
     public boolean isDFA() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'isDFA'");
+    }
+
+    /**
+     * Construct the textual representation of the DFA, for example
+     * A simple two state DFA
+     * Q = { a b }
+     * Sigma = { 0 1 }
+     * delta =
+     *		0	1
+     *	a	a	b
+     *	b	a	b
+     * q0 = a
+     * F = { b }
+     *
+     * The order of the states and the alphabet is the order
+     * in which they were instantiated in the DFA.
+     * @return String representation of the DFA
+     */
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        //Generating Q
+        builder.append(" Q = {");
+        for(NFAState c: states) {
+            builder.append(" " + c.getName());
+        }
+        builder.append(" }\n");
+
+        //Generating Sigma
+        builder.append("Sigma = {");
+        for(Character c: alphabet) {
+            builder.append(" " + c);
+        }
+        builder.append(" }\n");
+
+        //Generating Delta
+        builder.append("delta =" + "\n");
+        builder.append("\t");
+        for(Character c: alphabet) {
+            builder.append("\t" + c);
+        }
+        builder.append("\t\n");
+
+        for(NFAState c: states) {
+            builder.append("\t" + c.getName());
+            for(Character d: alphabet) {
+                //find transitions for all aphabet here and loop
+                builder.append("\t" + c.getToState(d));
+            }
+            builder.append("\n");
+        }
+
+        //Generating q0
+        builder.append("q0 =");
+        builder.append(" " + startState.getName() + "\n");
+
+        //Generating F
+        builder.append("F = {");
+        for(NFAState c: finalStates) {
+            builder.append(" " + c.getName());
+        }
+        builder.append(" }");
+
+        return builder.toString();
     }
 
 }
--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,9 +1,13 @@
 package fa.nfa;
+import java.util.HashMap;
+
 import fa.State;
 
 public class NFAState extends State {
+	private HashMap<Character,NFAState> transitionList;
 
-    public State() {
+    public NFAState() {
+		//super(name);
 		
 	}
 	
@@ -13,8 +17,9 @@ public class NFAState extends State {
 	 * This way name instance variable is 
 	 * correctly set.
 	 */
-	public State(String name) {
+	public NFAState(String name) {
 		this.name = name;
+        transitionList = new HashMap<Character,NFAState>();
 	}
 	
 	/**
@@ -22,12 +27,41 @@ public class NFAState extends State {
 	 * @return returns the state label.
 	 */
 	public String getName(){
-		return name;
+		return "";
+	}
+
+	/**
+     * Adds a transition to the DFAState's transition list.
+     * 
+     * @param symb - Character to transition on
+     * @param state - State to transition to
+     */
+    public void addToState(char symb, NFAState state) {
+        transitionList.put(symb, state);
+    }
+    
+    /**
+     * Returns the toState in the HashMap associated with the imput transition character.
+     *  
+     * @param symb - Transition character
+     * @return - The state that is paired to symb in the HashMap
+     */
+    public NFAState getToState(char symb) {
+        NFAState rval = transitionList.get(symb);
+        return rval;
+    }
+
+    /**
+     * Returns the TransitionList for the associated DFAState.
+     * @return - HashMap<Character, DFAState> transitionList
+     */
+    public HashMap<Character,NFAState> getTransitionList() {
+        return this.transitionList;
 	}
 	
 	@Override
 	public String toString(){
-		return name;
+		return "";
 	}
     
 }
