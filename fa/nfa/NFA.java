4f6fb0375eaf8782bcdc3e5dccb816225fefa29b
Munib Ahmed <munibahmed@u.boisestate.edu>
2023-10-24 00:00:27 -0600

    Fixed

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -23,20 +23,19 @@ import fa.State;
 // The NFA class implements the NFAInterface, indicating it uses methods specific to nondeterministic finite automata.
 public class NFA implements NFAInterface {
 
-
-// Class member variables representing the sets of states, final states, the start state, and the alphabet for the NFA.
-    private Set<NFAState> states;
+    // Class member variables representing the sets of states, final states, the
+    // start state, and the alphabet for the NFA.
     private Set<NFAState> states;
     private Set<NFAState> finalStates;
     private NFAState startState;
     private Set<Character> alphabet;
 
     /**
-     *Constructs an empty DFA object that is ready to be built.
+     * Constructs an empty DFA object that is ready to be built.
      */
     public NFA() {
-	
-	// Initialization of the NFA components
+
+        // Initialization of the NFA components
         startState = null;
         states = new LinkedHashSet<>();
         finalStates = new LinkedHashSet<>();
@@ -44,13 +43,15 @@ public class NFA implements NFAInterface {
     }
 
     /**
-	 * Adds a a state to the FA instance
-	 * @param name is the label of the state 
-	 * @return true if a new state created successfully and false if there is already state with such name
-	 */
+     * Adds a a state to the FA instance
+     * 
+     * @param name is the label of the state
+     * @return true if a new state created successfully and false if there is
+     *         already state with such name
+     */
     public boolean addState(String name) {
-        for(NFAState state: states) {
-            if(state.getName().equals(name)) {
+        for (NFAState state : states) {
+            if (state.getName().equals(name)) {
                 return false;
             }
         }
@@ -60,13 +61,14 @@ public class NFA implements NFAInterface {
     }
 
     /**
-	 * Marks an existing state as an accepting state
-	 * @param name is the label of the state
-	 * @return true if successful and false if no state with such name exists
-	 */
+     * Marks an existing state as an accepting state
+     * 
+     * @param name is the label of the state
+     * @return true if successful and false if no state with such name exists
+     */
     public boolean setFinal(String name) {
-        for(NFAState c: states) {
-            if(c.getName().equals(name)) {
+        for (NFAState c : states) {
+            if (c.getName().equals(name)) {
                 finalStates.add(c);
                 return true;
             }
@@ -75,12 +77,13 @@ public class NFA implements NFAInterface {
     }
 
     /**
-	 * Adds the initial state to the FA instance
-	 * @param name is the label of the start state
-	 * @return true if successful and false if no state with such name exists
-	 */
+     * Adds the initial state to the FA instance
+     * 
+     * @param name is the label of the start state
+     * @return true if successful and false if no state with such name exists
+     */
     public boolean setStart(String name) {
-        if(states.contains(getState(name))) {
+        if (states.contains(getState(name))) {
             startState = (NFAState) getState(name);
             return true;
         }
@@ -88,78 +91,82 @@ public class NFA implements NFAInterface {
     }
 
     /**
-	 * Adds a symbol to Sigma
-	 * @param symbol to add to the alphabet set
-	 */
+     * Adds a symbol to Sigma
+     * 
+     * @param symbol to add to the alphabet set
+     */
     public void addSigma(char symbol) {
         alphabet.add(symbol);
     }
 
     /**
-	 * Simulates a FA on input s to determine
-	 * whether the FA accepts s.
-	 * @param s - the input string
-	 * @return true if s in the language of the FA and false otherwise
-	 */
-    
+     * Simulates a FA on input s to determine
+     * whether the FA accepts s.
+     * 
+     * @param s - the input string
+     * @return true if s in the language of the FA and false otherwise
+     */
+
     public boolean accepts(String input) {
         NFAState current = startState;
         List<NFAState> visited = new ArrayList<>(); // To track state configurations
-    
+
         visited.add(current);
-    
+
         // Loop through the input string
         for (char symbol : input.toCharArray()) {
             // Create a new set to store next states
             Set<NFAState> nextStates = new LinkedHashSet<>();
-    
+
             // For each current state, find transitions for the symbol
             for (NFAState state : visited) {
                 Set<NFAState> stateTransitions = getToState(state, symbol);
                 nextStates.addAll(stateTransitions);
             }
-    
+
             if (nextStates.isEmpty()) {
                 // If there are no transitions, the NFA rejects the input string
                 return false;
             }
-    
-            // Update the current state with the set of next states
-           for(NFAState state: nextStates){
 
-            current = state;
-           }
-           // current = nextStates.iterator().next();
-    
+            // Update the current state with the set of next states
+            for (NFAState state : nextStates) {
+
+                current = state;
+            }
+            // current = nextStates.iterator().next();
+
             visited.addAll(nextStates);
         }
-    
+
         // After processing the entire input string, check for acceptance
         NFAState finalState = visited.get(visited.size() - 1);
-    
+
         if (isFinal(finalState.getName())) {
             return true; // The NFA accepts the input string
         }
-    
+
         return false; // The NFA rejects the input string
     }
-    
+
     /**
-	 * Getter for Sigma
-	 * @return the alphabet of FA
-	 */
+     * Getter for Sigma
+     * 
+     * @return the alphabet of FA
+     */
     public Set<Character> getSigma() {
         return alphabet;
     }
 
     /**
-	 * Returns state with the given name, or null if none exists
-	 * @param name of a state
-	 * @return state object or null
-	 */
+     * Returns state with the given name, or null if none exists
+     * 
+     * @param name of a state
+     * @return state object or null
+     */
     public State getState(String name) {
-        for(NFAState state: states) {
-            if(state.getName().equals(name)) {
+        for (NFAState state : states) {
+            if (state.getName().equals(name)) {
                 return state;
             }
         }
@@ -167,13 +174,14 @@ public class NFA implements NFAInterface {
     }
 
     /**
-	 * Determines if a state with a given name is final
-	 * @param name the name of the state
-	 * @return true if a state with that name exists and it is final
-	 */
+     * Determines if a state with a given name is final
+     * 
+     * @param name the name of the state
+     * @return true if a state with that name exists and it is final
+     */
     public boolean isFinal(String name) {
-        for(NFAState c: finalStates) {
-            if(c.getName().equals(name)) {
+        for (NFAState c : finalStates) {
+            if (c.getName().equals(name)) {
                 return true;
             }
         }
@@ -181,38 +189,40 @@ public class NFA implements NFAInterface {
     }
 
     /**
-	 * Determines if a state with name is final
-	 * @param name the name of the state
-	 * @return true if a state with that name exists and it is the start state
-	 */
+     * Determines if a state with name is final
+     * 
+     * @param name the name of the state
+     * @return true if a state with that name exists and it is the start state
+     */
     public boolean isStart(String name) {
         return startState.getName().equals(name);
     }
 
     /**
-	 * Return delta entries
-	 * @param from - the source state
-	 * @param onSymb - the label of the transition
-	 * @return a set of sink states
-	 */
+     * Return delta entries
+     * 
+     * @param from   - the source state
+     * @param onSymb - the label of the transition
+     * @return a set of sink states
+     */
     public Set<NFAState> getToState(NFAState from, char onSymb) {
         HashMap<Character, NFAState> temp = from.getTransitionList();
         Set<NFAState> rSet = new LinkedHashSet<>();
-        for (char symb: temp.keySet()) {
-            if(symb == onSymb) {
+        for (char symb : temp.keySet()) {
+            if (symb == onSymb) {
                 rSet.add(temp.get(onSymb));
             }
         }
         return rSet;
     }
 
-    
     /**
-	 * Traverses all epsilon transitions and determine
-	 * what states can be reached from s through e
-	 * @param state - NFA state to search for epsilon transitions
-	 * @return set of states that can be reached from s on epsilon trans.
-	 */
+     * Traverses all epsilon transitions and determine
+     * what states can be reached from s through e
+     * 
+     * @param state - NFA state to search for epsilon transitions
+     * @return set of states that can be reached from s on epsilon trans.
+     */
     public Set<NFAState> eClosure(NFAState state) {
         Set<NFAState> eClosureSet = new LinkedHashSet<>();// Instead of this, use a HashSet.
         Stack<NFAState> stack = new Stack<>();
@@ -228,16 +238,17 @@ public class NFA implements NFAInterface {
             eClosureSet.add(currentState);
 
             // Get the epsilon transitions of the current state
-            HashMap<Character,NFAState> tl = currentState.getTransitionList();
-            for(char c: tl.keySet()) {
-                if(c == 'e') {
+            HashMap<Character, NFAState> tl = currentState.getTransitionList();
+            for (char c : tl.keySet()) {
+                if (c == 'e') {
                     // Ad the state to the vector itself; don't do anything else.
                     Set<NFAState> temp = getToState(currentState, c);
                     // for(NFAState nfa: temp) {
-                    //     eClosureSet.add(nfa);
+                    // eClosureSet.add(nfa);
                     // }
                     eClosureSet.addAll(temp);
-                    for (NFAState nfa: temp) stack.push(nfa);
+                    for (NFAState nfa : temp)
+                        stack.push(nfa);
                 }
             }
         }
@@ -245,36 +256,45 @@ public class NFA implements NFAInterface {
     }
 
     /**
-	 * Determines the maximum number of NFA copies
-	 * created when processing string s
-	 * @param s - the input string
-	 * @return - the maximum number of NFA copies created.
-	 */
+     * Determines the maximum number of NFA copies
+     * created when processing string s
+     * 
+     * @param s - the input string
+     * @return - the maximum number of NFA copies created.
+     */
     public int maxCopies(String input) {
         return 0;
 
     }
 
     /**
-	 * Adds the transition to the NFA's delta data structure
-	 * @param fromState is the label of the state where the transition starts
-	 * @param toState is the set of labels of the states where the transition ends
-	 * @param onSymb is the symbol from the NFA's alphabet.
-	 * @return true if successful and false if one of the states don't exist or the symbol in not in the alphabet
-	 */
+     * Adds the transition to the NFA's delta data structure
+     * 
+     * @param fromState is the label of the state where the transition starts
+     * @param toState   is the set of labels of the states where the transition ends
+     * @param onSymb    is the symbol from the NFA's alphabet.
+     * @return true if successful and false if one of the states don't exist or the
+     *         symbol in not in the alphabet
+     */
     public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
-        if(onSymb == 'e' || !alphabet.contains(onSymb)) {alphabet.add('e');}
-        if(!alphabet.contains(onSymb)) {return false;}
-        if(!states.contains(getState(fromState))) {return false;}
-        for(String stateName: toStates) {
-            if(!states.contains(getState(stateName))) {
+        if (onSymb == 'e' || !alphabet.contains(onSymb)) {
+            alphabet.add('e');
+        }
+        if (!alphabet.contains(onSymb)) {
+            return false;
+        }
+        if (!states.contains(getState(fromState))) {
+            return false;
+        }
+        for (String stateName : toStates) {
+            if (!states.contains(getState(stateName))) {
                 return false;
             }
         }
         for (NFAState state : states) {
-            for(String toStateName : toStates) {
-                if(state.getName().equals(fromState)) {
-                    state.addToState(onSymb,(NFAState)getState(toStateName));
+            for (String toStateName : toStates) {
+                if (state.getName().equals(fromState)) {
+                    state.addToState(onSymb, (NFAState) getState(toStateName));
                     return true;
                 }
             }
@@ -282,25 +302,25 @@ public class NFA implements NFAInterface {
         return false;
     }
 
-    
     /**
-	 * Determines if NFA is an instance of a DFA
-	 * @return - true if NFA's transition function has DFA's properties.
-	 */
+     * Determines if NFA is an instance of a DFA
+     * 
+     * @return - true if NFA's transition function has DFA's properties.
+     */
     public boolean isDFA() {
-        for(NFAState state: states) {
+        for (NFAState state : states) {
             HashMap<Character, NFAState> temp = state.getTransitionList();
-            for (char symb: temp.keySet()) {
+            for (char symb : temp.keySet()) {
                 int flag = 0;
-                if(temp.containsKey(symb)) {
-                    flag +=1;
+                if (temp.containsKey(symb)) {
+                    flag += 1;
                 } else {
                     flag = 1;
-                } 
-                if(symb == 'e') {
+                }
+                if (symb == 'e') {
                     return false;
                 }
-                if(flag >=2) {
+                if (flag >= 2) {
                     return false;
                 }
             }
@@ -314,57 +334,58 @@ public class NFA implements NFAInterface {
      * Q = { a b }
      * Sigma = { 0 1 }
      * delta =
-     *		0	1
-     *	a	a	b
-     *	b	a	b
+     * 0 1
+     * a a b
+     * b a b
      * q0 = a
      * F = { b }
      *
      * The order of the states and the alphabet is the order
      * in which they were instantiated in the DFA.
+     * 
      * @return String representation of the DFA
      */
     public String toString() {
         StringBuilder builder = new StringBuilder();
 
-        //Generating Q
+        // Generating Q
         builder.append(" Q = {");
-        for(NFAState c: states) {
+        for (NFAState c : states) {
             builder.append(" " + c.getName());
         }
         builder.append(" }\n");
 
-        //Generating Sigma
+        // Generating Sigma
         builder.append("Sigma = {");
-        for(Character c: alphabet) {
+        for (Character c : alphabet) {
             builder.append(" " + c);
         }
         builder.append(" }\n");
 
-        //Generating Delta
+        // Generating Delta
         builder.append("delta =" + "\n");
         builder.append("\t");
-        for(Character c: alphabet) {
+        for (Character c : alphabet) {
             builder.append("\t" + c);
         }
         builder.append("\t\n");
 
-        for(NFAState c: states) {
+        for (NFAState c : states) {
             builder.append("\t" + c.getName());
-            for(Character d: alphabet) {
-                //find transitions for all aphabet here and loop
+            for (Character d : alphabet) {
+                // find transitions for all aphabet here and loop
                 builder.append("\t" + c.getToState(d));
             }
             builder.append("\n");
         }
 
-        //Generating q0
+        // Generating q0
         builder.append("q0 =");
         builder.append(" " + startState.getName() + "\n");
 
-        //Generating F
+        // Generating F
         builder.append("F = {");
-        for(NFAState c: finalStates) {
+        for (NFAState c : finalStates) {
             builder.append(" " + c.getName());
         }
         builder.append(" }");
